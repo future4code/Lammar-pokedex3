7a096f88d06f4a224e5bf3a3901de6c02c1b3c34
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-08-30 21:59:43 -0300

    Cria componente Card

--- pokedex/src/Components/Pages/Pokedex.js
+++ pokedex/src/Components/Pages/Pokedex.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export function Pokedex() {
+
+    const mockUpPic = {front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}
+
     return (
         <>
             <h1>Pokedex</h1>
