0f98c708f635cc6556f61cb493a5288b841935c0
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-01 22:36:21 -0300

    Endpoint get pokemon em progresso

--- pokedex/src/Components/Pages/HomePage.js
+++ pokedex/src/Components/Pages/HomePage.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { baseUrl } from "../Constants";
+import useRequestData from "../Hooks/useRequestData"
+
 
 export function HomePage() {
+
+    // const [pokemons, setPokemons] = useState([])
+    // let [pokemonImageUrl, setPokemonImageUrl] = useState('')
+
+    const [dataPokemons, isLoading, errorPokemon] = useRequestData(baseUrl)
+    
+    let pokemonImageUrl = ''
+    const renderPokemons = dataPokemons && dataPokemons.data.results.map((item, index) => {
+        
+        axios.get(`${baseUrl}${item.name}`).then(response => {
+            pokemonImageUrl = response.data.sprites.front_default
+            // setPokemonImageUrl(response.data.sprites.front_default)
+            // {console.log(response.data.sprites.front_default)}
+            // {console.log(pokemonImageUrl)}
+        })
+        
+        return (
+            <>
+                <p>{item.name}</p>
+                <img src={pokemonImageUrl} alt=""/>
+                {console.log(pokemonImageUrl)}
+            </>
+        )
+    })
+
     return (
         <>
             <h1>HomePage</h1>
+            {isLoading&&'Carregando...'}
+            {!isLoading&&dataPokemons&&
+                <>
+                    <ul>
+                        {renderPokemons}
+                    </ul>
+                </>
+            }
         </>
     );
 }
