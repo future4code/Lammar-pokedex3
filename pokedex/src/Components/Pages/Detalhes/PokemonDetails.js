8f28f68ba537484a8629882fc4a87260f66c65e5
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-09-06 22:14:16 -0300

    Renderização dos dados da pagina de detalhes

--- pokedex/src/Components/Pages/Detalhes/PokemonDetails.js
+++ pokedex/src/Components/Pages/Detalhes/PokemonDetails.js
@@ -1,21 +1,85 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, ButtonIcones, Header } from "./Style";
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useNavigate, useParams } from 'react-router-dom'
+import { Button, ButtonIcones, DivDados, DivDetalhes, DivImagemCostas, DivImagemFrontal, DivImagens,  DivMovimentos,  DivTipos,  Header } from "./Style"
 import { BiArrowBack } from 'react-icons/bi'
-import {CgPokemon} from 'react-icons/cg'
 
-export function PokemonDetails() {
+
+
+export const PokemonDetails=()=>{
     const navigate=useNavigate()
+    const param=useParams()
+    const [detalhes, setDetalhes]=useState()
 
+    useEffect(()=>{
+        requisicaoDetalhes()
+    }, [])
+
+    function requisicaoDetalhes(){
+        const UrlPokemon= `https://pokeapi.co/api/v2/pokemon/${param.name}`
+
+            axios.get(UrlPokemon).then((response)=>{
+            setDetalhes(response.data)
+            console.log(response.data)
+        })
+    }
     const voltar =()=>{
         navigate(-1)
     }
-    return (
+
+    return(
         <>
             <Header>
                 <ButtonIcones onClick={voltar}><BiArrowBack size="40px" /></ButtonIcones>
-                <Button> Adicionar/Remover da Pokedéx</Button>
+                <h1>{detalhes?.name[0].toUpperCase(0) + detalhes?.name.substr(1)}</h1>
+                <Button>Adicionar/Remover da pokedex</Button>
             </Header>
+            <DivDetalhes>
+                <DivImagens>
+                    <DivImagemFrontal
+/*                     style={detalhes?.types[0].type.name === "fire" ? {backgroundImage: r}:{}}
+ */
+                    >
+                        <h1>Imagem frontal:</h1>
+                        <img src={detalhes?.sprites.other.home.front_default} alt={detalhes?.name}/>
+                    </DivImagemFrontal>
+                    <DivImagemCostas>
+                        <h1>  Imagem de costas: </h1>
+                        <img src={detalhes?.sprites.back_default} alt={detalhes?.name}/>
+                    </DivImagemCostas>
+                </DivImagens>
+                <DivDados>
+                    <h1>Dados:</h1>
+                    <p>Experience: {detalhes?.base_experience}</p>   
+                    <p>XP: {detalhes?.stats[0].base_stat}</p>
+                    <p>Attack: {detalhes?.stats[1].base_stat}</p>
+                    <p>Defense: {detalhes?.stats[2].base_stat}</p>
+                    <p>Special Attack: {detalhes?.stats[3].base_stat}</p>
+                    <p>Special Defense: {detalhes?.stats[4].base_stat}</p>
+                    <p>Speed: {detalhes?.stats[5].base_stat}</p>
+                </DivDados>
+                <DivTipos>
+                    <p>Tipo: {detalhes?.types[0].type.name[0].toUpperCase(0) + detalhes?.types[0].type.name.substr(1)}</p>
+{/*                     <p>Tipo: {detalhes?.types[1].type.name[0].toUpperCase(0) + detalhes?.types[1].type.name.substr(1)}</p>
+ */}                </DivTipos>
+                <DivMovimentos>
+                    <h1>Movimentos</h1>
+                    <p>{detalhes?.moves[0].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[1].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[2].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[3].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[4].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[5].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[6].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[7].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[8].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                    <p>{detalhes?.moves[9].move.name[0].toUpperCase(0) + detalhes?.moves[0].move.name.substr(1)}</p>
+                </DivMovimentos>
+
+
+            </DivDetalhes>
+ 
+
         </>
-    );
+    )
 }
--- pokedex/src/Components/Pages/Detalhes/Style.js
+++ pokedex/src/Components/Pages/Detalhes/Style.js
@@ -1,23 +1,21 @@
-import styled from 'styled-components'
-import backgroundHeaderDetalhes from '../../../img/headerdetalhes.png'
+import styled from "styled-components";
+import backgroundHeaderDetalhes from "../../../img/headerdetalhes.png"
 
 export const Header=styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100vw;
+    width: 99vw;
     height: 10vh;
-    background-image: url(${backgroundHeaderDetalhes});
     background-color: #f6bd20;
-    background-size: contain;
-    background-position-x: center;
-    background-repeat: no-repeat;
-    opacity: 0.7;
-    :hover{
-        opacity: 1;
+    h1 {
+        font-family: 'Pokemon Solid', sans-serif;
+        color: #395A9F;
+        font-size: 40px;
+        font-weight: 100;
     }
-`
 
+`
 export const ButtonIcones=styled.button`
     margin: 0 20px;
     border: none;
@@ -30,7 +28,6 @@ export const ButtonIcones=styled.button`
         transition: .5s ease-in-out;
     }
 `
-
 export const Button=styled.button`
     margin: 0 20px;
     height: 5vh;
@@ -46,5 +43,135 @@ export const Button=styled.button`
         color: white;
         background-color: #f77a0f;
         transition: .5s ease-in-out;
+    }
+`
+
+export const DivDetalhes=styled.div`
+    display: flex;
+    max-height: 90vh;
+    width: 99vw;
+    flex-direction: column;
+    flex-wrap: wrap;
+    background-image: url(${backgroundHeaderDetalhes});
+    background-size: contain;
+    background-position-x: 99%;
+    background-repeat: no-repeat;
+    background-color: #f6bd208f;
+
+`
+export const DivImagens=styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 85vh;
+    width: 20%;
+    img{
+        width: 25vh;
+        :hover{
+            color: black;
+            -webkit-transform: scale(1.5);
+             transform: scale(1.5);
+             transition: .6s ease;
+        }
+    }
+
+    h1{
+        font-family: 'Rajdhani', sans-serif;
+        color: white;
+    }
+`
+
+export const DivImagemFrontal=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 30vh;
+    width: 80%;
+    border: 2px solid white;
+    margin-bottom: 5vh;
+    border-radius: 15px;
+    background-color: #f6bd20;
+`
+
+export const DivImagemCostas=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 30vh;
+    width: 80%;
+    border: 2px solid white;
+    border-radius: 15px;
+    background-color: #f6bd20;
+`
+
+export const DivDados=styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5vh;
+    height: 75vh;
+    width: 20%;
+    border: 2px solid white;
+    border-radius: 15px;
+    background-color: #f6bd20;
+
+    h1{
+        font-family: 'Rajdhani', sans-serif;
+        color: white;
+        text-align: center;
+    }
+
+    p{
+        font-family: 'Rajdhani', sans-serif;
+        margin-left: 20px;
+
+        font-size: 25px;
+        color: #395A9F;
+    }
+    
+`
+
+export const DivTipos=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 5vh;
+    width: 20%;
+    border: 2px solid white;
+    border-radius: 10px;
+    background-color: #f6bd20;
+
+    p{
+        margin-left: 10px;
+        font-family: 'Rajdhani', sans-serif;
+        font-size: 25px;
+        color: #395A9F;
+        text-align: center;
+    }
+`
+
+export const DivMovimentos=styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5vh;
+    height: 55vh;
+    width: 20%;
+    border: 2px solid white;
+    border-radius: 15px;
+    background-color: #f6bd20;
+
+    h1{
+        font-family: 'Rajdhani', sans-serif;
+        text-align: center;
+        color: white;
+    }
+    p{
+        font-family: 'Rajdhani', sans-serif;
+        font-size: 25px;
+        color: #395A9F;
+        text-align: start;
+        margin: 2px 30%;
     }
 `
